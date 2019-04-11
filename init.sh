d2700f38aa851b46d1dcd1e94cbe6f4f20bdfbf3
eliwyman <elliotwyman@gmail.com>
2019-04-11 21:42:48 +1000

    Added nginx-php config files. Added setup. Added init script comments

--- init.sh
+++ init.sh
@@ -1,12 +1,31 @@
 sudo apt-get update -y
+
+#nginx
 sudo apt-get install nginx -y
+
+#firewall settings
 sudo ufw allow 'Nginx HTTP'
 sudo ufw allow 'OpenSSH'
 sudo ufw --force enable
 sudo ufw status
 
+#sql
+#set sql p/w to allow silent install
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password your_password'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password your_password'
 sudo apt-get -y install mysql-server
 
+#to add
 #mysql_secure_installation
+
+#php
+sudo apt-get install php-fpm php-mysql -y
+sudo cp php.ini /etc/php/7.0/fpm/php.ini
+sudo systemctl restart php7.0-fpm
+
+#nginx - php
+sudo cp default /etc/nginx/sites-available/
+#config file debugging
+#sudo nginx -t
+sudo systemctl reload nginx
+
--- load.sh
+++ load.sh
@@ -1,2 +1,5 @@
 scp -r -i "/Users/el/Google Drive/login.pem" /Users/el/aws-deploy/init.sh $AWSUSR@$AWSHOST:/home/ubuntu/
 
+scp -r -i "/Users/el/Google Drive/login.pem" /Users/el/aws-deploy/php/php.ini $AWSUSR@$AWSHOST:/home/ubuntu/
+
+scp -r -i "/Users/el/Google Drive/login.pem" /Users/el/aws-deploy/nginx/default $AWSUSR@$AWSHOST:/home/ubuntu/
