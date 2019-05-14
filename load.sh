4d7fae8f89d0c9d6a7c67bffba927132ca5ea867
eliwyman <elliotwyman@gmail.com>
2019-05-14 21:52:49 +1000

    Added terraform files

--- load.sh
+++ load.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 scp -r -i "/Users/el/Google Drive/login.pem" /Users/el/aws-deploy/init.sh $AWSUSR@$AWSHOST:/home/ubuntu/
 
 scp -r -i "/Users/el/Google Drive/login.pem" /Users/el/aws-deploy/php/php.ini $AWSUSR@$AWSHOST:/home/ubuntu/
