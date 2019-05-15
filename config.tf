a174ca5f582a2d5902059a763a6562dbdfa151b1
El Wyman - Debian <elliotwyman@gmail.com>
2019-05-15 11:52:41 +1000

    Added user-data script; config for terraform

--- config.tf
+++ config.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region     = "us-east-2"
 }
 
+data "template_file" "user_data" {
+  template = "${file("${path.module}/userdata.tpl")}"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0653e888ec96eab9b"
   instance_type = "t2.nano"
---
+++ userdata.tpl
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#install Amazon Web Services Command Line Interface
+apt update
+apt install awscli -y
+
+# pull down initialization files from s3 bucket
+aws s3 cp s3://eliwyman-test/init.sh . --region us-east-2
+
+# run install scripts
+./init.sh
