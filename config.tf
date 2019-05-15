ffdef1470ba90cd6c4c7ab18c4594e5337bcc054
El Wyman - Debian <elliotwyman@gmail.com>
2019-05-15 12:19:16 +1000

    Moved reference to user_data to terraform config-resource

--- config.tf
+++ config.tf
@@ -2,12 +2,9 @@ provider "aws" {
   region     = "us-east-2"
 }
 
-data "template_file" "user_data" {
-  template = "${file("${path.module}/userdata.tpl")}"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0653e888ec96eab9b"
   instance_type = "t2.nano"
   key_name	= "login"
+  user_data	= "${file("userdata.tpl")}"
 }
