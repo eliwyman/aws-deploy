d2e88ae7c7a218ff5b2ebda76233ab4be67d8548
El Wyman - Debian <elliotwyman@gmail.com>
2019-05-20 22:59:31 +1000

    Added auto-scaling group to terraform config

--- config.tf
+++ config.tf
@@ -9,3 +9,15 @@ resource "aws_instance" "example" {
   user_data	= "${file("userdata.tpl")}"
   iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
 }
+
+resource "aws_autoscaling_group" "eliwyman_asg" {
+  availability_zones = ["us-east-2"]
+  desired_capacity   = 2
+  max_size           = 4
+  min_size           = 2
+
+  launch_template {
+    id      = "lt-0d2978ab4a1174ca1"
+    version = "$Latest"
+  }
+}
