ca0c36d5e4ca38cec79e46bef84227f28bb8ff1c
El Wyman - Debian <elliotwyman@gmail.com>
2019-07-11 21:59:45 +1000

    Uncommented certificate in alb listener, to allow HTTPS connections

--- config.tf
+++ config.tf
@@ -122,7 +122,7 @@ resource "aws_alb_listener" "listener_https" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#  certificate_arn   = "${var.certificate_arn}"
+  certificate_arn   = "${var.certificate_arn}"
   default_action {
     target_group_arn = "${aws_alb_target_group.group.arn}"
     type             = "forward"
